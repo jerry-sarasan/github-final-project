595ce711f4dd96c61388307b04900c208fc117a5
jerry-sarasan <109964065+jerry-sarasan@users.noreply.github.com>
2023-07-11 20:17:34 +0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # Jerry Sarasan
+   # jerry-sarasan
    # Input:
    # p, principal amount
    # t, time period in years
